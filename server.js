b6d7fdf16f5ab1ce476b5883a36bb227cf780ace
Matthew <bengethomasmatthew@gmail.com>
2020-10-11 17:48:48 +0100

    Push to heroku

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const cors = require("cors");
 //setup env
 require("dotenv").config();
 
-// force https on localhost
-const key = fs.readFileSync("./src/certs/selfsigned.key", "utf8");
-const cert = fs.readFileSync("./src/certs/selfsigned.crt", "utf8");
 const HTTP_PORT = 8080;
 const HTTPS_PORT = 8443;
 
@@ -26,6 +23,10 @@ app.use("/", routes);
 const { cron } = require("./src/common/cron");
 
 // here we go!
+
+if( "DEV" === process.env.ENV ){
+const key = fs.readFileSync("./src/certs/selfsigned.key", "utf8");
+const cert = fs.readFileSync("./src/certs/selfsigned.crt", "utf8");
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer({ key, cert }, app);
 
@@ -34,3 +35,10 @@ httpsServer.listen(HTTPS_PORT);
 
 console.log(`🚀 server running port ${HTTP_PORT}`);
 console.log(`🚀 server running port ${HTTPS_PORT}`);
+}else{
+app.listen(process.env.PORT || 3000, 
+    () => console.log("🚀 server running")
+);
+}
+
+
